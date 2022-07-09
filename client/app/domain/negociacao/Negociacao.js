2501ef79b0cc336d0a054a7192c67191f5ed64e9
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-07-09 14:13:32 -0300

    feat(jscangaceiro): criando e instânciando a Model negociacao

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,27 +1,36 @@
 class Negociacao {
-  #data
-  #quantidade
-  #valor
+  _data
+  _quantidade
+  _valor
+
+  get data () {
+    return new Date(this._data.getTime())
+  }
 
   get quantidade () {
-    return this.#quantidade ??= 0
+    return this._quantidade ??= 0
   }
 
   get valor () {
-    return this.#valor ??= 0
+    return this._valor ??= 0
   }
 
-  constructor (descriptor) {
-    this.#data = descriptor.data
-    this.#quantidade = descriptor.quantidade
-    this.#valor = descriptor.valor
-  }
-
-  obtemVolume () {
+  get volume () {
     return this.quantidade * this.valor
   }
 
-  static create (descriptor) {
-    return new Negociacao(descriptor)
+  constructor (_data, _quantidade, _valor) {
+    Object.assign(this, { _quantidade, _valor })
+    this._data = new Date(_data.getTime())
+    Object.freeze(this)
+    console.log(this)
+  }
+
+  static create (data, quantidade, valor) {
+    return new Negociacao(
+      data,
+      quantidade,
+      valor
+    )
   }
 }
