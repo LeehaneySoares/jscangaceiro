5dd0f88b8f0876ebb4f9f2289456bf1e54de2db5
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-07-12 20:17:07 -0300

    refactor(jscangaceiro): trazendo um design melhorado da class controller

--- client/app/controller/NegociacaoController.js
+++ client/app/controller/NegociacaoController.js
@@ -1,11 +1,43 @@
+import $ from './shortcut.js'
+import Negociacao from '../domain/negociacao/Negociacao.js'
 class NegociacaoController {
+  #data
+  #quantidade
+  #valor
+  #negociacao
+
+  get data () {
+    return this.#data
+  }
+
+  get quantidade () {
+    return this.#quantidade
+  }
+
+  get valor () {
+    return this.#valor
+  }
+
+  constructor (data, quantidade, valor) {
+    this.#data = data
+    this.#quantidade = quantidade
+    this.#valor = valor
+  }
+
   adiciona (event) {
     event.preventDefault()
-    alert('Chamei ação no controller')
+
+    this.#negociacao = Negociacao.create(this)
+
+    console.log(this.#negociacao)
   }
 
   static create () {
-    return new NegociacaoController()
+    return new NegociacaoController(
+      $('#data'),
+      $('#quantidade'),
+      $('#valor')
+    )
   }
 }
 
